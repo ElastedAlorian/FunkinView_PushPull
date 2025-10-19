2a29017f7639f5c9ff54a117a1b101718a96bcba
ElastedAlorian <82591291+SIG7Pro@users.noreply.github.com>
2025-10-19 15:51:48 -0400

    attempt at adding week 1 (its hard)

--- src/structures/gameplay/HealthBar.hx
+++ src/structures/gameplay/HealthBar.hx
@@ -111,7 +111,7 @@ class HealthBar {
 		iconP1.type = HEALTH_ICON;
 		iconP1.changeID(healthIconIDs[1][0]);
 		playerOGIcon = healthIconIDs[1][0];
-		trace(playerOGIcon + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		//trace(playerOGIcon[0] + "\n\n\n\n\n" + playerOGIcon[1] + "\n\n\n\n\n\n\n\n\n\n\n");
 
 		iconP2.y = iconP1.y = bg.y - 75;
 		iconP1.flip = true;
